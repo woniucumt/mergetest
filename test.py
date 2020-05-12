6b125ddd41f6fd9069aa83614f90293584bf9cee
2033912696@qq.com <LIUzheng11235831>
2020-05-12 10:21:21 +0800

    第一次修改

--- test.py
+++ test.py
@@ -1,14 +1,6 @@
 #the first file
 def fun1():
-	print("1")
-	print("1")
-	print("1444")
-	print("1")
 	print("我在这里修改2")
 
 def fun2():
-	print("1")
-	print("1")
-	print("1")
-	print("1")
 	print("我修改这里2")
